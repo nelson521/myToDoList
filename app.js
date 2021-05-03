e1ae55beb5dac9a4e228c7e990c690f0348b6874
Stevenson Nelson <sonson521@gmail.com>
2021-05-03 13:02:46 -0400

    add event handler.

--- app.js
+++ app.js
@@ -1,3 +1,22 @@
 let form = document.querySelector('#form');
 let task = document.querySelector('#task');
 let collection = document.querySelector('.collection');
+
+/* 
+  type something on the input 
+
+  click the submit button to submit the value in the input
+
+  it should appear on the collections area 
+  delete the item
+
+*/
+
+form.addEventListener('submit', function addTask(e) {
+  if (task.value === '') {
+    alert('Please fill out');
+  } else {
+    console.log(task.value);
+  }
+  e.preventDefault();
+});
